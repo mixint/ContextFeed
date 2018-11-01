83fa0facd143b904fe9fe470cb8e0ef7671a933b
Colten Jackson <jazzyjackson@gmail.com>
2018-10-31 19:48:19 -0500

    Added debug messages, fixed problem of setting inotify twice. Fixed problem of overwriting filename when there was no stat object.

--- ContextFeed.js
+++ ContextFeed.js
@@ -21,12 +21,16 @@ module.exports = class ContextFeed extends stream.Readable {
         this.options = options || {}
         this.pathname = pathname
         this.pathArray = new Array
-        debug(`Starting ContextFeed for ${this.pathname}`)
+        this.readystate = 0
+        debug(
+            `Starting ContextFeed for ${this.pathname}`
+        )
     }
 
     _read(size){
-        debug(`Read called. ArrayReady is ${this.arrayReady()}`)
-
+        debug(
+            `Read called. ArrayReady is ${this.arrayReady()}.`
+        )
         this.arrayReady() ? this.pushStat() : this.pause()
     }
 
@@ -35,53 +39,79 @@ module.exports = class ContextFeed extends stream.Readable {
         this.inotify && this.inotify.close()
     }
 
+
     arrayReady(){
-        // readystate: 
-        if(this.readdirFinished) return true
-        // else readdir and return undefined
+        // readystate: 0 is not started, 1 is not done, 2 is complete
+        if(this.readystate == 2) return true
+        if(this.readystate == 1) return false
+        else this.readystate = 1 // set readystate as 1, 'ongoing'
+
         fs.readdir(this.pathname, {
             withFileTypes: true
         }, (error, DirentArray) => {
-            error && this.emit('error', error)
-            debug(`ReadDir finished. ${DirentArray.length} entries.`)
-            this.readdirFinished = true
-            // extract Symbol(type) from any Dirent Object so I can use it as a key
+            this.readystate = 2 // flag readystate as complete
+            debug(
+                `ReadDir finished. ${DirentArray.length} entries. Sorting and adding '.' + '..'`
+            )
+            /**
+             * extract Symbol(type) from any Dirent Object so I can use it as a key
+             */
             const kType = Object.getOwnPropertySymbols(DirentArray[0])[0] // -> Symbol(type)
-            // sort DirEnt array by type consant. 0 is Unkown, 1 is File, 2 is Directory, etc.
-            // Compare b to a to reverse order from directory to file.
+            /**
+             * sort DirEnt array by type consant. 0 is Unkown, 1 is File, 2 is Directory, etc.
+             * Compare b to a to reverse order from directory to file.
+             */
             DirentArray.sort((a,b) => b[kType] - a[kType]) 
 
             this.pathArray = ['.','..'].concat(DirentArray.map(Dirent => Dirent.name))
             this.pushStat()
 
             this.inotify = fs.watch(this.pathname, (event, filename) => {
-                // only push new pathnames
-                debug(`inotify callback. Event: ${event}, Filename: ${filename}`)
-
+                debug(
+                    `inotify callback. Event: ${event}, Filename: ${filename}`
+                )
                 if(!this.pathArray.includes(filename)){
                     this.pathArray.push(filename)
                     this.pushStat()
                 }
             })
+
             this.inotify.on('error', error => this.emit("error", error))
             this.on('end', () => this.inotify.close()) // close FS.watcher when a stream is closed
-        })
-
+        }) // return undefined, coerced to false, pauses stream
     }
 
     pushStat(){
         if(this.pathArray.length == 0){
-            debug(`pathArray empty on pushStat. keepAlive is ${Boolean(this.options.keepAlive)}.`)
-            this.options.keepAlive ? this.pause() : this.push(null) // it's done
+            debug(
+                `pushStat called, pathArray is empty. keepAlive is ${Boolean(this.options.keepAlive)}, ` +
+                `${this.options.keepAlive ? `pausing ContextFeed.` : `ending ContextFeed.`}`
+            )
+            if(this.options.keepAlive){
+                this.pause()
+            } else {
+                this.push(null) // it's done
+            }
         } else {
-            debug(`pathArray has ${this.pathArray.length} entries on pushStat.`)
+            debug(
+                `pushStat called, pathArray has ${this.pathArray.length} entries.`
+            )
             let filename = this.pathArray.pop()
             extraStat(path.join(this.pathname, filename), (err, stat) => {
-                stat.filename = filename // overwrite with original name, specially for '.' and '..'
+                debug(
+                    `extraStat finished, ${err ? `Err being pushed.` : `Stat being pushed.`}`
+                )
                 // if pushStat was called while paused, resume.
-                this.isPaused() && this.resume()
                 // if push returns true, push again, else wait.
-                this.push(err || stat) ? this.pushStat() : this.pause()
+                if(this.isPaused()){
+                    this.resume()
+                }
+                if(err){
+                    this.push(err) ? this.pushStat() : this.pause()
+                } else {
+                    Object.assign(stat, {filename})
+                    this.push(stat) ? this.pushStat() : this.pause()
+                }
             })
         }
     }
